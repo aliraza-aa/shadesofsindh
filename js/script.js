c8b3396576f76f4ca398db4004cdce2fa22c5997
Ali Raza Desktop <alirp366@gmail.com>
2021-09-22 03:27:49 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+    $(".designers-slider").slick({
+        dots: false,
+        infinite: true,
+        speed: 1000,
+        slidesToShow: 1,
+        prevArrow: '<button class="slide-arrow prev-arrow"></button>',
+        nextArrow: '<button class="slide-arrow next-arrow"></button>'
+    });
+
+});
